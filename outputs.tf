d1bc2e4d103e0f89cb80455315c1a6589200c053
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-08 16:16:56 -0300

    fix output

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,15 @@
-output "repository_name" {
+output "name" {
   description = "Name of the ECR Repository"
   value       = aws_ecr_repository.ecr_repository.name
+}
+
+output "arn" {
+  description = "Name of the ECR Repository"
+  value       = aws_ecr_repository.ecr_repository.arn
+}
+
+output "registry_id" {
+  description = "Name of the ECR Repository"
+  value       = aws_ecr_repository.ecr_repository.registry_id
 }
 
