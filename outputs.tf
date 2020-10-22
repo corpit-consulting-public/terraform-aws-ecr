4cecd862894baf9b12bf36cdba9a97c795865216
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-10-22 17:38:39 -0300

    rollback output

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,4 @@
-output "name" {
+output "repository_name" {
   description = "Name of the ECR Repository"
   value       = aws_ecr_repository.ecr_repository.name
 }
-
-output "arn" {
-  description = "Name of the ECR Repository"
-  value       = aws_ecr_repository.ecr_repository.arn
-}
-
-output "registry_id" {
-  description = "Name of the ECR Repository"
-  value       = aws_ecr_repository.ecr_repository.registry_id
-}
-
